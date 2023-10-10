4187a4633a7007af18342d0149501e6f37abfbb2
Sebastian Fermanelli <sebastianfermanelli@gmail.com>
2023-10-09 22:36:34 -0300

    Cambio de motor de base de datos a PostgreSQL

--- src/config/db/database.connection.ts
+++ src/config/db/database.connection.ts
@@ -4,13 +4,17 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const Config: Options = {
-	dialect: 'mysql',
+	dialect: 'postgres',
 	host: process.env.DB_HOST,
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
 	port: Number(process.env.DB_PORT),
-	logging: false,
 };
 
-export const sequelize: Sequelize = new Sequelize(Config);
+export const sequelize: Sequelize = new Sequelize(
+	`${Config.dialect}://${Config.username}:${Config.password}@${Config.host}:${Config.port}/${Config.database}?sslmode=require`,
+	{
+		logging: false,
+	}
+);
